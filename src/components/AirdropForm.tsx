6a9e280edf69da09f32af1d38cc0bde9e0db1cba
siddhant-k08 <kulkarni.sid0809@gmail.com>
2025-12-12 19:42:38 +0530

    state hooks setup

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -5,6 +5,14 @@ import { InputForm } from "./ui/InputField"
 
 export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("")
+    const [recipients, setRecipient] = useState("")
+    const [amounts, setAmounts] = useState("")
+
+    async function handleSubmit() {
+        console.log(tokenAddress)
+        console.log(recipients)
+        console.log(amounts)
+    }
 
     return (
         <div>
@@ -14,6 +22,22 @@ export default function AirdropForm() {
                 value={tokenAddress}
                 onChange={e => setTokenAddress(e.target.value)}
             />
+            <InputForm
+                label="Recipients"
+                placeholder="0x123, 0x456"
+                value={recipients}
+                onChange={e => setRecipient(e.target.value)}
+                large={true}
+            />
+            <InputForm
+                label="Amount"
+                placeholder="100,200,300..."
+                value={amounts}
+                onChange={e => setAmounts(e.target.value)}
+            />
+            <button onClick={handleSubmit}>
+                Send tokens
+            </button>
         </div>
     )
 }
