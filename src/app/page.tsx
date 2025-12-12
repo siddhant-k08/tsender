da7805e6efc252d0b40e5fcbca3097e05aed5c81
siddhant-k08 <kulkarni.sid0809@gmail.com>
2025-12-13 01:47:51 +0530

    e2e tests added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import AirdropForm from "../components/AirdropForm";
+"use client"
+
+import HomeContent from "../components/HomeContent";
+import { useAccount } from "wagmi"
 
 export default function Home() {
+  const { isConnected } = useAccount()
   return (
     <div>
-      <AirdropForm />
+      {isConnected ? (
+        <div>
+          <HomeContent />       
+        </div>
+      ) : (
+        <div>
+          Please connect a wallet!
+        </div>
+      )}
     </div>
   );
 }
